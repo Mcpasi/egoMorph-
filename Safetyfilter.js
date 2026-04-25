358829f654d85af9fc9a547d1c5926fc11fb9deb
Mcpasi <kuspas29@gmail.com>
2026-04-25 18:25:01 +0200

    Update Safetyfilter.js

--- Safetyfilter.js
+++ Safetyfilter.js
@@ -12,3 +12,20 @@
  * `window.SafetyFilter` und wird von chatModel.js automatisch genutzt,
  * sofern verfügbar.
  */
+(function () {
+    'use strict';
+    // Wortliste für Deutsch + Englisch. Bewusst klein gehalten und auf klar
+    // anstößige Begriffe (Beleidigungen, Gewalt, Sexualisierung, Hass)
+    // beschränkt. Keine Stoppwort-Listen oder politische Begriffe.
+    const BLOCKED_TERMS = [
+        // Beleidigungen / Hass (DE)
+        'arschloch', 'arschlöcher', 'wichser', 'fotze', 'fotzen', 'hure', 'huren',
+        'hurensohn', 'hurensöhne', 'schlampe', 'schlampen', 'missgeburt',
+        'missgeburten', 'spast', 'spasti', 'spastiker', 'mongo', 'mongos',
+        'krüppel', 'behindi', 'kanake', 'kanaken', 'nigger', 'neger',
+        'judensau', 'untermensch', 'untermenschen',
+        // Gewalt / Drohung (DE)
+        'umbringen', 'töten', 'erschießen', 'erstechen', 'vergewaltigen',
+        'vergewaltigung', 'abschlachten', 'massakrieren',
+        // Sexualisierung / explizit (DE)
+      
